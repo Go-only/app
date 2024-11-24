99e91af81962f8b3f07b712122ff557e1e776d23
Go-only <107931677+Go-only@users.noreply.github.com>
2024-11-24 23:38:15 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import WebApp from "@twa-dev/sdk";
 
 function App() {
+  useEffect(() => {
+    WebApp.ready();
+    WebApp.setHeaderColor("secondary_bg_color");
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="App"
+      style={{
+        padding: "20px",
+        color: WebApp.themeParams.text_color,
+        backgroundColor: WebApp.themeParams.bg_color,
+      }}
+    >
+      <h1>Mining Game</h1>
+      <p>Coming soon...</p>
     </div>
   );
 }
